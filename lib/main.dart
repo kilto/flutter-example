848e1d578c0614c9ea892937e30625de3f7e28a4
kilto <kilto2@hotmail.com>
2021-04-06 19:45:02 +0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,46 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: const Text('Hello World'),
-        ),
-      ),
+      home: Text('Hello World'),
     );
   }
 }
+
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Welcome to Flutter',
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Welcome to Flutter'),
+//         ),
+//         body: const Center(
+//           child: const Text('Hello World'),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         title: 'Welcome to Flutter',
+//         home: Scaffold(
+//             body:Container(
+//                 height: 400,
+//                 child: SizedBox.expand(
+//                   child: CupertinoTimerPicker(
+//                     mode: CupertinoTimerPickerMode.hm,
+//                     minuteInterval: 1,
+//                     secondInterval: 1,
+//                     onTimerDurationChanged: (Duration changedtimer) {
+//                     },
+//                   ),
+//                 )
+//             )));
+//   }
+// }
--- lib/main4.dart
+++ lib/main4.dart
@@ -22,7 +22,7 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   @override
-  final _suggestions = <WordPair>[];                 // NEW
+  final _suggestions = generateWordPairs().take(5).toList();                 // NEW
   final _biggerFont = const TextStyle(fontSize: 18); // NEW
 
   Widget build(BuildContext context) {
@@ -36,16 +36,23 @@ class _RandomWordsState extends State<RandomWords> {
 
   Widget _buildRow(WordPair pair) {
     return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
+      title: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            pair.asPascalCase,
+            style: _biggerFont,
+          ),
+          Divider()
+        ],
+      )
     );
   }
 
   Widget _buildSuggestions() {
     return ListView.builder(
         padding: const EdgeInsets.all(16),
+        itemCount: _suggestions.length,
         // The itemBuilder callback is called once per suggested
         // word pairing, and places each suggestion into a ListTile
         // row. For even rows, the function adds a ListTile row for
@@ -55,22 +62,23 @@ class _RandomWordsState extends State<RandomWords> {
         itemBuilder: (BuildContext _context, int i) {
           // Add a one-pixel-high divider widget before each row
           // in the ListView.
-          if (i.isOdd) {
-            return Divider();
-          }
+          // if (i.isOdd) {
+          //   return Divider();
+          // }
 
           // The syntax "i ~/ 2" divides i by 2 and returns an
           // integer result.
           // For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
           // This calculates the actual number of word pairings
           // in the ListView,minus the divider widgets.
-          final int index = i ~/ 2;
+          // final int index = i ~/ 2;
+          final int index = i;
           // If you've reached the end of the available word
           // pairings...
           if (index >= _suggestions.length) {
             // ...then generate 10 more and add them to the
             // suggestions list.
-            _suggestions.addAll(generateWordPairs().take(10));
+            // _suggestions.addAll(generateWordPairs().take(10));
           }
           return _buildRow(_suggestions[index]);
         }
